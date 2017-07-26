d6dade7c8566e3386882b244a73b9317c673e5fa
emorenodtt <emorenomillan@deloitte.es>
2017-07-26 11:25:59 +0200

    Scroll menu added

--- src/app/components/app-home/app-home.component.ts
+++ src/app/components/app-home/app-home.component.ts
@@ -23,13 +23,16 @@ export class AppHomeComponent implements OnInit {
 
     openNav() {
         if (!this.menuActive) {
-            $('#myNav').height('100%');
+            $('body,html').css('overflow-y','hidden');
+            $('.overlay').css('top', $('#header').height());
+            $('#myNav').height($(window).height() - $('#header').height());
             $('#logo').attr('src',  '../../../assets/AOD-Logo-Responsive.png');
             this.menuActive = !this.menuActive;
             $('#nav').attr('class',  'navbar navbar-toggleable-md bg-inverse');
             this.srcLogin = '../../../assets/Boton-Acceso-Usuarios-gris.png';
             this.srcMenu = '../../../assets/Boton-Salir-Menu-Responsive-OFF.png';
         } else {
+            $('body,html').css('overflow-y','auto');
             $('#myNav').height('0%');
             $('#nav').attr('class', 'navbar navbar-toggleable-md bg-light');
             $('#logo').attr('src',  '../../../assets/AOD-Logo.png');
