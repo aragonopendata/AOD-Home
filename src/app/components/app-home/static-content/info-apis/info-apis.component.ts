1e0124296b2090abf0005159cee7e85670eabe11
galcober-dtt <galcober@deloitte.es>
2017-07-27 11:10:45 +0200

    Revert "Info apis +/- changed"
    This reverts commit c99268523c89832457dd4300ad6cd21633805cb4.

--- src/app/components/app-home/static-content/info-apis/info-apis.component.ts
+++ src/app/components/app-home/static-content/info-apis/info-apis.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import $ from 'jquery';
 
 @Component({
   selector: 'app-info-apis',
@@ -8,43 +7,9 @@ import $ from 'jquery';
 })
 export class InfoApisComponent implements OnInit {
 
-    activated: boolean = false;
-    idActive: String = '';
+  constructor() { }
 
-    constructor() { }
-
-    ngOnInit() {
-        let i: number = 0;
-        $("i").each(function () {
-            $(this).attr('id_', i);
-            i++;
-        });
-    }
-
-    getChildren(id) {
-        if (this.idActive.localeCompare(id) === 0 || this.idActive.localeCompare('') === 0) {
-            this.activated = !this.activated;
-            this.changeIcon($(id).children().children());
-            if (this.idActive.localeCompare(id) === 0) {
-                this.idActive = '';
-            } else {
-                this.idActive = id;
-            }
-        } else {
-            this.activated = !this.activated;
-            this.changeIcon($(this.idActive).children().children());
-            this.activated = !this.activated;
-            this.changeIcon($(id).children().children());
-            this.idActive = id;
-        }
-    }
-
-    changeIcon(tag) {
-        if (this.activated) {
-            $(tag).attr('class', 'fa fa-minus-circle');
-        } else {
-            $(tag).attr('class', 'fa fa-plus-circle');
-        }
-    }
+  ngOnInit() {
+  }
 
 }
