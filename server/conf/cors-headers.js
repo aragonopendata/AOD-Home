76b80f1d936712c65c387abe593710ecfc893a55
jortizsisamon-dtt <jortizsisamon@deloitte.es>
2017-09-22 09:51:12 +0200

    New response headers added
    New response headers added to allow CORS from browsers

--- server/conf/cors-headers.js
+++ server/conf/cors-headers.js
@@ -1,8 +1,8 @@
 function crosPermission(){
     this.permission = function(req, res, next) {
         res.header('Access-Control-Allow-Origin','*');
-        res.header('Access-Control-Allow-Headers','Content-Type');
-        res.header('Access-Control-Allow-Methods','GET','POST','PUT','DELETE','OPTIONS');
+        res.header('Access-Control-Allow-Headers','Access-Control-Allow-Headers, Access-Control-Allow-Methods, Access-Control-Allow-Origin, Content-Type, Origin, X-Requested-With, Accept, Authorization');
+        res.header('Access-Control-Allow-Methods','GET, POST, PUT, DELETE, PATCH, OPTIONS');
         next();
     }
 }
