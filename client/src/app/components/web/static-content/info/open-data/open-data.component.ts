03d03e2fd8fc8c2bf0195a2dff54cf1ad0148be8
jbarrio-dtt <jbarrio@deloitte.es>
2017-10-09 17:36:49 +0200

    Go to section in OpenDataInfo, Changes in Dataset Detail

--- client/src/app/components/web/static-content/info/open-data/open-data.component.ts
+++ client/src/app/components/web/static-content/info/open-data/open-data.component.ts
@@ -1,31 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewChecked  } from '@angular/core';
 import { AccordionModule } from 'primeng/primeng';
 import { StaticContent } from '../../../../../models/StaticContent';
 import { StaticContentService } from '../../../../../services/web/static-content.service';
 import { Router, ActivatedRoute } from '@angular/router';
+import $ from 'jquery';
 
 @Component({
     selector: 'app-open-data',
     templateUrl: './open-data.component.html',
     styleUrls: ['./open-data.component.css']
 })
-export class OpenDataComponent implements OnInit {
+export class OpenDataComponent implements OnInit, AfterViewChecked {
     index: number = 0;
     contents: StaticContent[];
     sectionTitle: string;
     sectionSubtitle: string;
     sectionDescription: string;
     targetUrl: string;
+    url: string = null;
 
     constructor(private staticContentService: StaticContentService, private activatedRoute: ActivatedRoute) { }
 
     ngOnInit() {
         this.getStaticContentInfo();
+        
     }
 
     getUrlFragment() {
         this.activatedRoute.fragment.subscribe(fragment => {
-			this.targetUrl = fragment;
+            this.targetUrl = fragment;
 		});
     }
 
@@ -44,5 +47,20 @@ export class OpenDataComponent implements OnInit {
                 });
             }
         });
+    }
+
+    ngAfterViewChecked() {
+        this.openSection();
+    }
+
+    openSection(){
+        if(this.targetUrl != this.url && this.targetUrl != null){
+            this.url = this.targetUrl;
+            document.getElementById(this.url+'Link').setAttribute('aria-expanded','true');
+            document.getElementById(this.url+'Link').setAttribute('class','headLink');
+            document.getElementById(this.url).setAttribute('class','collapse show');
+            var element = document.getElementById(this.url+'Link');
+            $("html, body").animate({ scrollTop: $(element).offset().top - 200}, '500');
+        }
     }
 }
