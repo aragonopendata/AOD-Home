1ac436f61eca633870a439c003194cdaa75955b7
Julio <julio88.99@gmail.com>
2017-12-19 13:44:02 +0100

    OnClick Fix

--- client/src/app/components/web/static-content/services/analytics/analytics.component.ts
+++ client/src/app/components/web/static-content/services/analytics/analytics.component.ts
@@ -22,13 +22,12 @@ export class AnalyticsComponent {
 	constructor(private logstashService: AnalyticsService) { }
 
 	ngOnInit() {
-		this.getFiles();
 		var days = 90;
 		var portal = "Todos";
 
 		jQuery("#analytics").attr('src', get_url(get_time(days), get_filter(portal)));
-
-		jQuery(".timeAnalytics").click(function (event) {
+		
+		jQuery('.iframeAnalytics').on("click", ".timeAnalytics", function(event) {
 			event.preventDefault();
 			jQuery('#btng-time-analytics button.active').removeClass('active');
 			jQuery(this).addClass('active')
@@ -36,7 +35,7 @@ export class AnalyticsComponent {
 			jQuery("#analytics").attr('src', get_url(get_time(days), get_filter(portal)));
 		});
 
-		jQuery(".portalAnalytics").click(function (event) {
+		jQuery('.iframeAnalytics').on("click", ".portalAnalytics", function(event) {
 			event.preventDefault();
 			jQuery('.portalAnalyticsPral').html(jQuery(this).val() + ' <span class="caret"></span>');			
 			portal = jQuery(this).val();
@@ -60,6 +59,8 @@ export class AnalyticsComponent {
 			var url = "http://172.27.38.119:7030/elastic/app/kibana#/dashboard/e6433860-d68c-11e7-a49d-f956d0989e2c?embed=true&_g=(refreshInterval:(display:Off,pause:!f,value:0),time:" + time + ")&_a=(description:'descripcion+descripcion',filters:!(" + filter + "),fullScreenMode:!f,options:(darkTheme:!f),panels:!((col:1,id:'534087c0-d682-11e7-a49d-f956d0989e2c',panelIndex:3,row:16,size_x:12,size_y:6,type:visualization),(col:1,id:'2b1be890-de87-11e7-9b2b-757dce07ac8f',panelIndex:13,row:6,size_x:12,size_y:6,type:visualization),(col:1,id:'3a794cb0-d9dc-11e7-9b2b-757dce07ac8f',panelIndex:14,row:12,size_x:6,size_y:2,type:visualization),(col:1,id:'98511a60-de88-11e7-9b2b-757dce07ac8f',panelIndex:15,row:1,size_x:12,size_y:3,type:visualization),(col:1,id:da0124b0-d9dc-11e7-9b2b-757dce07ac8f,panelIndex:16,row:4,size_x:12,size_y:2,type:visualization),(col:7,id:'7d1730b0-d9d6-11e7-9b2b-757dce07ac8f',panelIndex:17,row:12,size_x:6,size_y:2,type:visualization),(col:1,id:'32b9c250-de89-11e7-9b2b-757dce07ac8f',panelIndex:18,row:14,size_x:6,size_y:2,type:visualization),(col:7,id:'4cfa6e90-d9dd-11e7-9b2b-757dce07ac8f',panelIndex:19,row:14,size_x:6,size_y:2,type:visualization)),query:(language:lucene,query:''),timeRestore:!t,title:'Test+DashBoard',uiState:(P-13:(vis:(params:(sort:(columnIndex:!n,direction:!n)))),P-15:(vis:(defaultColors:('0+-+100':'rgb(0,104,55)'))),P-16:(spy:(mode:(fill:!f,name:!n))),P-3:(mapCenter:!(40.34654412118006,-3.8012695312500004),mapZoom:6)),viewMode:view)";
 			return url;
 		}
+
+		this.getFiles();
 	}
 
 	getFiles() {
