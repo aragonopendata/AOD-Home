dd4aa19029d971b31258c2ce73a142c64b6499b9
Julio <julio88.99@gmail.com>
2018-01-08 09:48:43 +0100

    link position

--- client/src/app/app.constants.ts
+++ client/src/app/app.constants.ts
@@ -675,9 +675,9 @@ export class Constants {
 
     public static KIBANA_URL = Constants.AOD_BASE_URL + "/elastic/app/kibana#/dashboard/e6433860-d68c-11e7-a49d-f956d0989e2c";
     public static KIBANA_URL_SEVEN = Constants.AOD_BASE_URL + "/elastic/app/kibana#/dashboard/6b50d7f0-e589-11e7-bac3-69701294f3ab";
+
     public static ELASTIC_BROWSERS = Constants.AOD_BASE_URL + "/gapi/get_browsers_elastic";
     public static ELASTIC_FILES = Constants.AOD_BASE_URL + "/gapi/get_files_elastic";
-
     public static ELASTIC_PAGES = Constants.AOD_BASE_URL + "/gapi/get_pages_elastic";
     public static ELASTIC_COUNTRIES = Constants.AOD_BASE_URL + "/gapi/get_countries_elastic";
 }
