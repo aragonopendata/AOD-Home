79b433f51c6e4876285a26122c30805c7b4ff4eb
galcober-dtt <galcober@deloitte.es>
2019-08-12 11:15:32 +0200

    TTL formart added

--- client/src/app/app.constants.ts
+++ client/src/app/app.constants.ts
@@ -421,6 +421,7 @@ export class Constants {
     public static ADMIN_DATASET_EDIT_DROPDOWN_FORMATS_XLSX = { label: 'XLSX', value: '24' };
     public static ADMIN_DATASET_EDIT_DROPDOWN_FORMATS_XML = { label: 'XML', value: '25' };
     public static ADMIN_DATASET_EDIT_DROPDOWN_FORMATS_ZIP = { label: 'ZIP', value: '26' };
+    public static ADMIN_DATASET_EDIT_DROPDOWN_FORMATS_TTL = { label: 'TTL', value: '27' };
     
     
     public static ADMIN_DATASET_EDIT_DROPDOWN_ACCESS_MODES_CSV = { label: 'CSV', value: '1' };
@@ -449,6 +450,7 @@ export class Constants {
     public static ADMIN_DATASET_EDIT_DROPDOWN_ACCESS_MODES_XLSX = { label: 'XLSX', value: '24' };
     public static ADMIN_DATASET_EDIT_DROPDOWN_ACCESS_MODES_XML = { label: 'XML', value: '25' };
     public static ADMIN_DATASET_EDIT_DROPDOWN_ACCESS_MODES_ZIP = { label: 'ZIP', value: '26' };
+    public static ADMIN_DATASET_EDIT_DROPDOWN_ACCESS_MODES_TTL = { label: 'TTL', value: '27' };
 
     public static ADMIN_DATASET_ERR_LOAD_DATASET = '200 - undefined';
     
